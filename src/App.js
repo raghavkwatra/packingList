83b95e23e826b263edd2346e16cf90b7966ad5b8
raghavkwatra <rkwatra454@gmail.com>
2025-08-15 16:51:53 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌊Far Away🌊</h1>;
+}
+
+function Form() {
+  return <div className="add-form">What items you want to add?</div>;
+}
+
+function PackingList() {
+  return <h3 className="list">List</h3>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      You have X items in your list ,and you have alredy packed X (X%) items
+    </footer>
+  );
+}
